ceb9641485533da2aea635c55ba5796121d1242f
Chloe Chan <chanc7@berkeley.edu>
2022-10-16 05:53:48 -0700

    integrated ToDo into app

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,14 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
+import {TextField , Button } from '@mui/material';
+import Todo from './components/Todo';
 import "./Dashboard.css";
 import { auth, db, logout } from "./firebase";
-import { query, collection, getDocs, where } from "firebase/firestore";
+import { query, collection, getDocs, where, orderBy, onSnapshot, addDoc,serverTimestamp, getDoc } from "firebase/firestore";
+
+const q=query(collection(db,'foods'),orderBy('timestamp','desc'));
 
 function Dashboard() {
   const [user, loading, error] = useAuthState(auth);
   const [name, setName] = useState("");
   const navigate = useNavigate();
+  const [todos,setTodos]=useState([]);
+  const [input, setInput]=useState('');
 
   const fetchUserName = async () => {
     try {
@@ -23,15 +29,44 @@ function Dashboard() {
     }
   };
 
+
+
   useEffect(() => {
     if (loading) return;
     if (!user) return navigate("/");
 
     fetchUserName();
-  }, [user, loading]);
+    //fetchFoods();
+
+    onSnapshot(q,(snapshot)=>{
+      setTodos(snapshot.docs.map(doc=>({
+        id: doc.id,
+        item: doc.data()
+      })))
+    });
+  }, [user, loading, input]);
+
+  const addTodo=(e)=>{
+    e.preventDefault();
+       addDoc(collection(db,'foods'),{
+         foodname:input,
+         timestamp: serverTimestamp()
+       })
+       console.log('click')
+      setInput('')
+  };
+  console.log(todos);
 
   return (
     <div className="dashboard">
+      <form>
+         <TextField id="outlined-basic" label="Make Todo" variant="outlined" style={{margin:"0px 5px"}} size="small" value={input}
+         onChange={e=>setInput(e.target.value)} />
+        <Button variant="contained" color="primary" onClick={addTodo}  >Add Todo</Button>
+      </form>
+      <ul>
+          {todos.map(item=> <Todo key={item.id} arr={item} />)}
+      </ul>
       <div className="dashboard__container">
         Logged in as
         <div>{name}</div>
--- src/firebase.js
+++ src/firebase.js
@@ -18,13 +18,13 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIXJ5YT7hoNbBFqK3TBcV41-TzIO-7n7w",
-  authDomain: "fir-auth-6edd8.firebaseapp.com",
-  projectId: "fir-auth-6edd8",
-  storageBucket: "fir-auth-6edd8.appspot.com",
-  messagingSenderId: "904760319835",
-  appId: "1:904760319835:web:44fd0d957f114b4e51447e",
-  measurementId: "G-Q4TYKH9GG7",
+  apiKey: "AIzaSyA-fatAtD8wWlUK5-MY4-HBZkXmRZvZyH8",
+  authDomain: "calhacks-2022-1b778.firebaseapp.com",
+  projectId: "calhacks-2022-1b778",
+  storageBucket: "calhacks-2022-1b778.appspot.com",
+  messagingSenderId: "53313502929",
+  appId: "1:53313502929:web:b7d8e10a7dabcac2c00ff0",
+  measurementId: "G-70J0GJ2SFM"
 };
 
 const app = initializeApp(firebaseConfig);
